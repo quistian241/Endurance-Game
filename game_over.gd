70bcc0097ec026147aa2c19214fca5368577b72b
quistian241 <97694494+quistian241@users.noreply.github.com>
2026-03-27 13:49:28 -0700

    Game Over screen improved
    Added functional "Retry" & "Main Menu" buttons onto the "Game Over" screen.

---
+++ game_over.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	hide()
+
+func unpause_game():
+	get_tree().paused = false
+
+func _on_retry_pressed() -> void:
+	unpause_game()
+	get_tree().reload_current_scene()
+	hide()
+	
+
+
+func _on_main_menu_pressed() -> void:
+	unpause_game()
+	get_tree().change_scene_to_file("res://main_menu.tscn")
